5bd2fe9537338c9fdf428522f29a87d125907f8f
Quan95974@ <quankietsuat@gmail.com>
2026-05-28 13:56:28 +0700

    disable failing test for deploy

--- src/main/java/com/example/banckend/BanckendApplication.java
+++ src/main/java/com/example/banckend/BanckendApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+
 public class BanckendApplication {
 
 	public static void main(String[] args) {
